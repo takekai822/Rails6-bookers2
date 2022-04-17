1caa56243b29a5296457b33626911fe5de1496df
takekai822 <kaikisoccer78@gmail.com>
2022-04-17 09:49:58 +0000

    [Update]bookers2

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,5 +1,4 @@
 class BookCommentsController < ApplicationController
-
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
@@ -12,7 +11,6 @@ class BookCommentsController < ApplicationController
     BookComment.find(params[:id]).destroy
     redirect_back(fallback_location: root_path)
   end
-
 
   private
   def book_comment_params
